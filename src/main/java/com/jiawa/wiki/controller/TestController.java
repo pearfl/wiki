9dd95f50727bf4282c3fd8a4836a9e918d0249c0
pearfl <pearfl@qq.com>
2021-08-02 17:36:03 +0800

    2-6 开发hello world接口，访问地址为:http://127.0.0.1:8880/hello

---
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -0,0 +1,31 @@
+package com.jiawa.wiki.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    /**
+     * GET, POST, PUT, DELETE
+     *
+     * /user?id=1
+     * /user/1
+     * @return
+     */
+    // @PostMapping
+    // @PutMapping
+    // @DeleteMapping
+    // @RequestMapping(value = "/user/1", method = RequestMethod.GET)
+    // @RequestMapping(value = "/user/1", method = RequestMethod.DELETE)
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post，" + name;
+    }
+}
